e48d6c8508d1582be3d938ffc57db5c5d0327ac4
mathias3 <maciej.osowski@gmail.com>
2017-01-18 23:29:03 +0000

    Create dataquest-refactoring.py

---
+++ python  -third-try/dataquest-refactoring.py
@@ -0,0 +1,13 @@
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm with the default paramters
+# n_estimators is the number of trees we want to make
+# min_samples_split is the minimum number of rows we need to make a split
+# min_samples_leaf is the minimum number of samples we can have at the place where a tree branch ends (the bottom points of the tree)
+alg = RandomForestClassifier(random_state=1, n_estimators=10, min_samples_split=2, min_samples_leaf=1)
+kf = cross_validation.KFold(titanic.shape[0], n_folds=3, random_state=1)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=kf)
+print (scores.mean())
