0e4b4b0157ac62424f2da83e78c8a4d36cdb8f34
mathias3 <maciej.osowski@gmail.com>
2017-01-18 23:35:27 +0000

    Update dataquest-refactoring.py

--- python  -third-try/dataquest-refactoring.py
+++ python  -third-try/dataquest-refactoring.py
@@ -7,7 +7,13 @@ predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 # n_estimators is the number of trees we want to make
 # min_samples_split is the minimum number of rows we need to make a split
 # min_samples_leaf is the minimum number of samples we can have at the place where a tree branch ends (the bottom points of the tree)
-alg = RandomForestClassifier(random_state=1, n_estimators=10, min_samples_split=2, min_samples_leaf=1)
-kf = cross_validation.KFold(titanic.shape[0], n_folds=3, random_state=1)
+alg = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=8, min_samples_leaf=3)
+kf = cross_validation.KFold(titanic.shape[0], n_folds=10, random_state=1)
 scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=kf)
 print (scores.mean())
+
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
